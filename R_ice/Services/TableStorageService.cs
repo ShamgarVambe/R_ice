ab202fe55026e69f1c6aae808a3009c6b3cad24a
ShamgarVambe <128706559+ShamgarVambe@users.noreply.github.com>
2025-09-09 14:13:19 +0200

    Add project files.

---
+++ R_ice/Services/TableStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+
+namespace R_ice.Services
+{
+    public class TableStorageService
+    {
+    }
+}
