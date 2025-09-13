aadd9840e80ea4f521ca2a06175b8e0d439b07d5
ShamgarVambe <128706559+ShamgarVambe@users.noreply.github.com>
2025-09-13 10:21:25 +0200

    added proper storage connection

--- R_ice/Function1.cs
+++ R_ice/Function1.cs
@@ -1,24 +1,26 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace R_ice
+public class Function1
 {
-    public class Function1
+    private readonly ILogger<Function1> _logger;
+    private readonly string _storageConnectionString;
+    public Function1(ILogger<Function1> logger)
     {
-        private readonly ILogger<Function1> _logger;
+        _logger = logger;
+        _storageConnectionString = Environment.GetEnvironmentVariable("connection");
+    }
 
-        public Function1(ILogger<Function1> logger)
-        {
-            _logger = logger;
-        }
-
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
-        }
+    [Function("Function1")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
     }
 }
+
